9f77527e0b5e3022e5f8e714ce033117fbf2c7aa
Amaaq <maaqoul.adil@gmail.com>
2023-08-10 11:48:07 +0100

    testing 123

--- app.js
+++ app.js
@@ -30,3 +30,26 @@ mongoose
   .then(()=>{
     console.log("MongoDB connection is established successfully!")
   })
+
+let sendRequest = document.querySelector("#fetch")
+  sendRequest.addEventListener('click',async (e)=>{
+    e.preventDefault()
+    let data = await fetch('https://amaaq.github.io/authAPI/auth/signin',{
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({
+            email : "maaqoul.adil@gmail.com",
+            password : "123456789"
+        }), // body data type must match "Content-Type" header
+      });
+      let response = data.json()
+      console.log(response)
+    })
